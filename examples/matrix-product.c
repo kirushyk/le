6e9eb0b0ccf1a65c4092bf6dcae371210c554422
Kyrylo Polezhaiev <kirushyk@gmail.com>
2017-12-26 00:46:28 +0200

    examples: Add matrix product demo

---
+++ examples/matrix-product.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "le.h"
+
+int
+main(int argc, const char *argv[])
+{
+    LeMatrix *a = le_matrix_new_rand(4, 2);
+    printf("a =\n");
+    le_matrix_print(a, stdout);
+    
+    LeMatrix *b = le_matrix_new_rand(2, 4);
+    printf("b =\n");
+    le_matrix_print(b, stdout);
+    
+    LeMatrix *c = le_matrix_new_product(a, b);
+    printf("c = a * b =\n");
+    le_matrix_print(c, stdout);
+    
+    LeMatrix *d = le_matrix_new_transpose(c);
+    printf("d = c' =\n");
+    le_matrix_print(d, stdout);
+    
+    le_matrix_free(d);
+    le_matrix_free(c);
+    le_matrix_free(b);
+    le_matrix_free(a);
+    
+    return 0;
+}
