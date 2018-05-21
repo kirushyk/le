c21fac68e29c73989a76550d2ca8c10757f4b80b
Kyrylo Polezhaiev <kirushyk@gmail.com>
2018-05-21 09:55:29 +0300

    le: Add dummy basic object

---
+++ le/leobject.h
@@ -0,0 +1,13 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#ifndef __LE_OBJECT_H__
+#   define __LE_OBJECT_H__
+
+typedef struct LeObject LeObject;
+
+LeObject * le_object_alloc (void);
+
+void       le_object_free  (LeObject *);
+
+#endif
