6e1c9b2cd3fdb24090ef8d8e2fa37dc2859b4b4c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-08-29 18:22:39 +0200

    sequential: Add proper class initialization and constructor

--- le/lesequential.c
+++ le/lesequential.c
@@ -9,10 +9,39 @@ struct LeSequential
     
 };
 
+typedef struct LeSequentialClass
+{
+    LeModelClass parent;
+} LeSequentialClass;
+
+LeSequentialClass le_sequential_class;
+
+static void
+le_sequential_class_ensure_init()
+{
+    static int le_sequential_class_initialized = 0;
+    
+    if (!le_sequential_class_initialized)
+    {
+        le_sequential_class.parent.predict =
+        (LeMatrix *(*)(LeModel *, LeMatrix *))le_sequential_predict;
+        le_sequential_class_initialized = 1;
+    }
+}
+
+void
+le_sequential_construct(LeSequential *self)
+{
+    le_model_construct((LeModel *)self);
+    le_sequential_class_ensure_init();
+    ((LeObject *)self)->klass = (LeClass *)&le_sequential_class;
+}
+
 LeSequential *
 le_sequential_new(void)
 {
     LeSequential *self = malloc(sizeof(struct LeSequential));
+    le_sequential_construct(self);
     return self;
 }
 
@@ -23,7 +52,8 @@ le_sequential_predict(LeSequential *self, LeMatrix *x)
     return y;
 }
 
-void le_sequential_free(LeSequential *self)
+void
+le_sequential_free(LeSequential *self)
 {
     free(self);
 }
