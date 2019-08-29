f8779c7b1862b9431e02aa356a0c40c5e28e6361
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-08-29 17:38:10 +0200

    model: Add assertions for predict call

--- le/lemodel.c
+++ le/lemodel.c
@@ -1,6 +1,7 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
+#include <assert.h>
 #include "lemodel.h"
 
 LeModelClass le_model_class;
@@ -13,6 +14,10 @@ void le_model_construct(LeModel *model)
 LeMatrix *
 le_model_predict(LeModel *self, LeMatrix *x)
 {
+    assert(self);
+    assert(((LeObject *)self)->klass);
+    assert(((LeModelClass *)((LeObject *)self)->klass)->predict);
+    
     return ((LeModelClass *)((LeObject *)self)->klass)->predict(self, x);
 }
 
