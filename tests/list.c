09430bf85d33f268017516f60b7fa26959e098be
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-09-04 05:16:03 +0200

    tests: Improve tests for List

--- tests/list.c
+++ tests/list.c
@@ -15,5 +15,22 @@ main()
     assert(a->prev == NULL);
     assert(a->data == (void *)0x12345678);
     
+    LeList *b = le_list_append(a, (void *)0x87654321);
+    
+    assert(b);
+    assert(b == a);
+    assert(a->next != NULL);
+    assert(a->prev == NULL);
+    assert(a->data == (void *)0x12345678);
+    
+    LeList *c = le_list_last(b);
+    
+    assert(c);
+    assert(c != a);
+    assert(c->next == NULL);
+    assert(c->prev == a);
+    assert(c == a->next);
+    assert(c->data == (void *)0x87654321);
+    
     return EXIT_SUCCESS;
 }
