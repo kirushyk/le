3f26a82200d66dd3f62c896ede33c3fda1e2c32c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-09-06 20:11:09 +0200

    layer: Add dummy struct and activation type enumeration

---
+++ le/lelayer.h
@@ -0,0 +1,23 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+ Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#ifndef __LELAYER_H__
+#define __LELAYER_H__
+
+#include "lematrix.h"
+
+typedef enum LeActivation
+{
+    LE_ACTIVATION_NONE,
+    LE_ACTIVATION_SIGMOID,
+    LE_ACTIVATION_TANH,
+    LE_ACTIVATION_RELU,
+} LeActivation;
+
+struct LeLayer
+{
+    LeMatrix *weights;
+    LeActivation activation;
+};
+
+#endif
