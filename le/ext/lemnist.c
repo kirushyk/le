e15910fd1efc8a22edebe9161ff1d9c330aa31ed
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-01 15:43:34 +0200

    mnist: Add dummy header and source file

---
+++ le/ext/lemnist.c
@@ -0,0 +1,4 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include "lemnist.h"
---
+++ le/ext/lemnist.h
@@ -0,0 +1,9 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#ifndef __LEMNIST_H__
+#define __LEMNIST_H__
+
+
+
+#endif
