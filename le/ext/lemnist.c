7414bd63ce7f27b6c09d609d29865807ff8e451d
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-03 20:58:53 +0200

    mnist: Implement allocation and cleanup

--- le/ext/lemnist.c
+++ le/ext/lemnist.c
@@ -2,15 +2,24 @@
 Released under the MIT license. See LICENSE file in the project root for full license information. */
 
 #include "lemnist.h"
+#include <stdlib.h>
 
 MNIST *
 le_mnist_load(const char *path)
 {
-    return NULL;
+    MNIST *mnist = malloc(sizeof(MNIST));
+    mnist->test = NULL;
+    mnist->train = NULL;
+    return mnist;
 }
 
 void
 le_mnist_free(MNIST *mnist)
 {
-    
+    if (mnist)
+    {
+        le_data_set_free(mnist->train);
+        le_data_set_free(mnist->test);
+        free(mnist);
+    }
 }
