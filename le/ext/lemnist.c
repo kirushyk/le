eee7b00e6c4569c0ec54c762cd37cbcd19b41d4c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-03 20:40:30 +0200

    mnist: Add dummy struct and functions

--- le/ext/lemnist.c
+++ le/ext/lemnist.c
@@ -2,3 +2,15 @@
 Released under the MIT license. See LICENSE file in the project root for full license information. */
 
 #include "lemnist.h"
+
+MNIST *
+le_mnist_load(const char *path)
+{
+    return NULL;
+}
+
+void
+le_mnist_free(MNIST *mnist)
+{
+    
+}
--- le/ext/lemnist.h
+++ le/ext/lemnist.h
@@ -1,9 +1,19 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
+#include "../ledataset.h"
+
 #ifndef __LEMNIST_H__
 #define __LEMNIST_H__
 
+typedef struct MNIST
+{
+    LeDataSet *train;
+    LeDataSet *test;
+} MNIST;
 
+MNIST * le_mnist_load (const char *path);
+
+void    le_mnist_free (MNIST      *mnist);
 
 #endif
