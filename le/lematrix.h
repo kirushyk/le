a6dc40a7613bc9055a623cf74b70dc4163f7b36f
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-04 05:55:27 +0200

    matrix: Include data type header

--- le/lematrix.h
+++ le/lematrix.h
@@ -1,6 +1,8 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
+#include "letype.h"
+
 #ifndef __LEMATRIX_H__
 #define __LEMATRIX_H__
 
