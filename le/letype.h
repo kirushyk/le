4f85604079508a81d5c954c7026ab00d0c0791df
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-04 05:54:52 +0200

    type: Add data type enumeration

---
+++ le/letype.h
@@ -0,0 +1,13 @@
+//
+//  letype.h
+//  le
+//
+//  Created by Cyril on 11/4/19.
+//  Copyright © 2019 kirushyk. All rights reserved.
+//
+
+#ifndef letype_h
+#define letype_h
+
+
+#endif /* letype_h */
