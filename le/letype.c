4d6ba00c92d7ccc8cd1b3129a152d4a715c8ce14
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-05 03:51:31 +0200

    type: Add size getter

---
+++ le/letype.c
@@ -0,0 +1,24 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include "letype.h"
+
+size_t
+le_type_size(LeType type)
+{
+    switch (type) {
+    case LE_TYPE_INT8:
+    case LE_TYPE_UINT8:
+        return 1;
+    case LE_TYPE_INT16:
+        return 2;
+    case LE_TYPE_INT32:
+    case LE_TYPE_FLOAT32:
+        return 4;
+    case LE_TYPE_FLOAT64:
+        return 8;
+    case LE_TYPE_VOID:
+    default:
+        return 0;
+    }
+}
--- le/letype.h
+++ le/letype.h
@@ -1,6 +1,8 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
+#include <stddef.h>
+
 #ifndef __LETYPE_H__
 #define __LETYPE_H__
 
@@ -14,5 +16,7 @@ typedef enum LeType
     LE_TYPE_FLOAT32,
     LE_TYPE_FLOAT64
 } LeType;
+
+size_t le_type_size(LeType type);
 
 #endif
