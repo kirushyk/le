7d621492eac171841e56947b3ca69054f48084a3
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-11 05:01:52 +0200

    mnist: Fix train / test mismatch

--- le/ext/lemnist.c
+++ le/ext/lemnist.c
@@ -14,15 +14,15 @@ le_mnist_load(const char *path)
     MNIST *mnist = malloc(sizeof(MNIST));
     char buffer[1024];
     
-    sprintf(buffer, "%s/t10k-images-idx3-ubyte", path);
+    sprintf(buffer, "%s/train-images-idx3-ubyte", path);
     LeTensor *input = le_idx_read(buffer);
-    sprintf(buffer, "%s/t10k-labels-idx1-ubyte", path);
+    sprintf(buffer, "%s/train-labels-idx1-ubyte", path);
     LeTensor *output = le_idx_read(buffer);
     mnist->train = le_data_set_new_take(input, output);
-    
-    sprintf(buffer, "%s/train-images-idx3-ubyte", path);
+
+    sprintf(buffer, "%s/t10k-images-idx3-ubyte", path);
     input = le_idx_read(buffer);
-    sprintf(buffer, "%s/train-labels-idx1-ubyte", path);
+    sprintf(buffer, "%s/t10k-labels-idx1-ubyte", path);
     output = le_idx_read(buffer);
     mnist->test = le_data_set_new_take(input, output);
     
