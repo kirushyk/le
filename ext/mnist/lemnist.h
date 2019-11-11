fa01597fa8fa22a5ec101a533852d5eced3c00e0
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-12 00:27:58 +0200

    mnist: Move to ext/ subfolder

--- ext/mnist/lemnist.h
+++ ext/mnist/lemnist.h
@@ -1,7 +1,7 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
-#include "../ledataset.h"
+#include <le/le.h>
 
 #ifndef __LEMNIST_H__
 #define __LEMNIST_H__
