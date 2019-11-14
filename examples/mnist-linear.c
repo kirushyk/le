abc2728dd727ef7710f652b0581a33782fe7d03c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-14 16:54:09 +0200

    mnist-linear: Convert label matrices to ONE HOT output

--- examples/mnist-linear.c
+++ examples/mnist-linear.c
@@ -18,15 +18,19 @@ main()
     LeTensor *train_input_f32 = le_tensor_new_cast_f32(train_input);
     LeTensor *train_labels = le_data_set_get_output(mnist->train);
     le_tensor_reshape(train_labels, 2, 1, 60000);
+    LeTensor *train_output = le_matrix_new_one_hot(train_labels, 10);
     LeTensor *test_images = le_data_set_get_input(mnist->test);
     le_tensor_reshape(test_images, 2, 10000, 28 * 28);
     LeTensor *test_input = le_matrix_new_transpose(test_images);
     LeTensor *test_input_f32 = le_tensor_new_cast_f32(test_input);
     LeTensor *test_labels = le_data_set_get_output(mnist->test);
     le_tensor_reshape(test_labels, 2, 1, 10000);
+    LeTensor *test_output = le_matrix_new_one_hot(test_labels, 10);
     
+    le_tensor_free(test_output);
     le_tensor_free(test_input_f32);
     le_tensor_free(test_input);
+    le_tensor_free(train_output);
     le_tensor_free(train_input_f32);
     le_tensor_free(train_input);
     le_mnist_free(mnist);
