7f7d6fa3ecc1e77f54b885f531cb081f6257cf46
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-16 11:58:15 +0200

    mnist-1-layer-nn: Use one-hot output vectors

--- examples/mnist/mnist-1-layer-nn.c
+++ examples/mnist/mnist-1-layer-nn.c
@@ -18,18 +18,18 @@ main()
     LeTensor *train_input_f32 = le_tensor_new_cast_f32(train_input);
     LeTensor *train_labels = le_data_set_get_output(mnist->train);
     le_tensor_reshape(train_labels, 2, 1, 60000);
-    LeTensor *train_output = le_tensor_new_f32_equal_u8(train_labels, 5);
+    LeTensor *train_output = le_matrix_new_one_hot(train_labels, 10);
     LeTensor *test_images = le_data_set_get_input(mnist->test);
     le_tensor_reshape(test_images, 2, 10000, 28 * 28);
     LeTensor *test_input = le_matrix_new_transpose(test_images);
     LeTensor *test_input_f32 = le_tensor_new_cast_f32(test_input);
     LeTensor *test_labels = le_data_set_get_output(mnist->test);
     le_tensor_reshape(test_labels, 2, 1, 10000);
-    LeTensor *test_output = le_tensor_new_f32_equal_u8(test_labels, 5);
+    LeTensor *test_output = le_matrix_new_one_hot(test_labels, 10);
     
     Le1LayerNN *classifier = le_1_layer_nn_new();
     Le1LayerNNTrainingOptions options;
-    options.max_iterations = 500;
+    options.max_iterations = 10;
     options.alpha = 0.03f;
     le_1_layer_nn_train(classifier, train_input_f32, train_output, options);
     le_1_layer_nn_free(classifier);
