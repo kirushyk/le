63242ede435a1000df91b22a0a1664822a7b4c7c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-20 23:50:04 +0200

    examples: Add Gradient Descent optimization demo

---
+++ examples/optimization/gradient-descent.c
@@ -0,0 +1,33 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include <stdlib.h>
+#include <math.h>
+#include <le/le.h>
+#include <le/optimization/legradientdescent.h>
+
+float
+J(float w)
+{
+    return 1 + (w - 3) * (w - 3);
+}
+
+float
+dJdw(float w)
+{
+    return 2 * (w - 3);
+}
+
+int
+main(int argc, const char *argv[])
+{
+    LeTensor *w = le_scalar_new_f32(0.0f);
+    LeList *parameters = le_list_append(NULL, w);
+    LeGradientDescent *optimizer = le_gradient_descent_new(parameters, 0.01f);
+    unsigned i;
+    for (i = 0; i < 10; i++)
+    {
+        le_optimizer_step((LeOptimizer *)optimizer);
+    }
+    return EXIT_SUCCESS;
+}
