5175ad1044c799b0a7ff3275eca737c04700524a
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-11-21 04:39:52 +0200

    examples: Pass gradiens to optimizer

--- examples/optimization/gradient-descent.c
+++ examples/optimization/gradient-descent.c
@@ -23,15 +23,22 @@ main(int argc, const char *argv[])
 {
     LeTensor *w = le_scalar_new_f32(0.0f);
     LeList *parameters = le_list_append(NULL, w);
-    LeGradientDescent *optimizer = le_gradient_descent_new(parameters, 0.01f);
+    
+    LeTensor *dw = le_scalar_new_f32(0.0f);
+    LeList *gradients = le_list_append(NULL, dw);
+    
+    LeGradientDescent *optimizer = le_gradient_descent_new(parameters, 0.2f);
+    ((LeOptimizer *)optimizer)->gradients = gradients;
+    
     unsigned i;
-    for (i = 0; i < 10; i++)
+    for (i = 0; i < 20; i++)
     {
         printf("Iteration %u. ", i);
         le_optimizer_step((LeOptimizer *)optimizer);
         float w_ = le_tensor_f32_at(w, 0);
-        float grad = dJdw(w_);
-        printf("J(%0.2f) = %0.2f. Gradient = %0.2f\n", w_, J(w_), grad);
+        le_tensor_f32_set(dw, 0, dJdw(w_));
+        float grad_ = le_tensor_f32_at(dw, 0);
+        printf("J(%0.2f) = %0.2f. Gradient = %0.2f\n", w_, J(w_), grad_);
     }
     return EXIT_SUCCESS;
 }
