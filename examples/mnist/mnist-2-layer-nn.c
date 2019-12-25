1eecd3b615820caeed676f98de8d57886a5b7d6a
Kyrylo Polezhaiev <kirushyk@gmail.com>
2019-12-25 17:54:41 +0200

    examples: Add SVM and 2-layer NN model demos for MNIST dataset

---
+++ examples/mnist/mnist-2-layer-nn.c
@@ -0,0 +1,68 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <le/le.h>
+#include <le/letensor-imp.h>
+#include <ext/mnist/lemnist.h>
+
+int
+main()
+{
+    MNIST *mnist = le_mnist_load(NULL);
+
+    LeTensor *train_images = le_data_set_get_input(mnist->train);
+    le_tensor_reshape(train_images, 2, 60000, 28 * 28);
+    LeTensor *train_input = le_matrix_new_transpose(train_images);
+    LeTensor *train_input_f32 = le_tensor_new_cast_f32(train_input);
+    LeTensor *train_labels = le_data_set_get_output(mnist->train);
+    le_tensor_reshape(train_labels, 2, 1, 60000);
+    LeTensor *train_output = le_matrix_new_one_hot(train_labels, 10);
+    LeTensor *test_images = le_data_set_get_input(mnist->test);
+    le_tensor_reshape(test_images, 2, 10000, 28 * 28);
+    LeTensor *test_input = le_matrix_new_transpose(test_images);
+    LeTensor *test_input_f32 = le_tensor_new_cast_f32(test_input);
+    LeTensor *test_labels = le_data_set_get_output(mnist->test);
+    le_tensor_reshape(test_labels, 2, 1, 10000);
+    LeTensor *test_output = le_matrix_new_one_hot(test_labels, 10);
+    
+    LeSequential *neural_network = le_sequential_new();
+    le_sequential_add(neural_network, );
+    // le_1_layer_nn_init(neural_network, 28 * 28, 10);
+    LeBGD *optimizer = le_bgd_new(le_model_get_parameters(LE_MODEL(neural_network)),
+                                  0.03f);
+    for (unsigned i = 0; i <= 2500; i++)
+    {
+        LeList *gradients = le_model_get_gradients(LE_MODEL(neural_network),
+                                                   train_input_f32, train_output);
+        LE_OPTIMIZER(optimizer)->gradients = gradients;
+        le_optimizer_step(LE_OPTIMIZER(optimizer));
+        le_list_foreach(gradients, (LeFunction)le_tensor_free);
+        
+        if (i % 100 == 0) {
+            printf("Iteration %d.\n", i);
+            
+            LeTensor *train_prediction = le_model_predict(LE_MODEL(neural_network), train_input_f32);
+            float train_set_error = le_one_hot_misclassification(train_prediction, train_output);
+            printf("Train Set Error: %f\n", train_set_error);
+            le_tensor_free(train_prediction);
+
+            LeTensor *test_prediction = le_model_predict(LE_MODEL(neural_network), test_input_f32);
+            float test_set_error = le_one_hot_misclassification(test_prediction, test_output);
+            printf("Test Set Error: %f\n", test_set_error);
+            le_tensor_free(test_prediction);
+        }
+    }
+    
+    le_sequential_free(neural_network);
+    le_tensor_free(test_output);
+    le_tensor_free(test_input_f32);
+    le_tensor_free(test_input);
+    le_tensor_free(train_output);
+    le_tensor_free(train_input_f32);
+    le_tensor_free(train_input);
+    le_mnist_free(mnist);
+
+    return EXIT_SUCCESS;
+}
---
+++ examples/mnist/mnist-svm.c
@@ -0,0 +1,46 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <le/le.h>
+#include <le/letensor-imp.h>
+#include <ext/mnist/lemnist.h>
+
+int
+main()
+{
+    MNIST *mnist = le_mnist_load(NULL);
+
+    LeTensor *train_images = le_data_set_get_input(mnist->train);
+    le_tensor_reshape(train_images, 2, 60000, 28 * 28);
+    LeTensor *train_input = le_matrix_new_transpose(train_images);
+    LeTensor *train_input_f32 = le_tensor_new_cast_f32(train_input);
+    LeTensor *train_labels = le_data_set_get_output(mnist->train);
+    le_tensor_reshape(train_labels, 2, 1, 60000);
+    LeTensor *train_output = le_tensor_new_f32_equal_u8(train_labels, 5);
+    LeTensor *test_images = le_data_set_get_input(mnist->test);
+    le_tensor_reshape(test_images, 2, 10000, 28 * 28);
+    LeTensor *test_input = le_matrix_new_transpose(test_images);
+    LeTensor *test_input_f32 = le_tensor_new_cast_f32(test_input);
+    LeTensor *test_labels = le_data_set_get_output(mnist->test);
+    le_tensor_reshape(test_labels, 2, 1, 10000);
+    LeTensor *test_output = le_tensor_new_f32_equal_u8(test_labels, 5);
+    
+    LeSVM *classifier = le_svm_new();
+    LeSVMTrainingOptions options;
+    options.kernel = LE_KERNEL_RBF;
+    options.c = 1;
+    le_svm_train(classifier, train_input_f32, train_output, options);
+    le_svm_free(classifier);
+    
+    le_tensor_free(test_output);
+    le_tensor_free(test_input_f32);
+    le_tensor_free(test_input);
+    le_tensor_free(train_output);
+    le_tensor_free(train_input_f32);
+    le_tensor_free(train_input);
+    le_mnist_free(mnist);
+
+    return EXIT_SUCCESS;
+}
