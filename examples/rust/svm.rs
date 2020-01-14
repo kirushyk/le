d62fe81330568112672f1b993adf7ea20e30f2a7
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-01-14 03:02:43 +0200

    examples: Add SVM demo in Rust and Python

---
+++ examples/rust/svm.rs
@@ -0,0 +1,9 @@
+// Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+// Released under the MIT license. See LICENSE file in the project root for full license information.
+
+use le::Tensor;
+
+fn main() {
+    let x = Tensor::new();
+    print!("x = {}", x);
+}
