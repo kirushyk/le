d6b5341341b7f28ec019c4699c3d0d94b5c1b726
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-01-31 13:42:25 +0200

    tests/sobel: Check conv2d operation

--- tests/sobel.c
+++ tests/sobel.c
@@ -1,19 +1,15 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
+#define DEFAULT_LOG_CATEGORY "sobel"
+
 #include <stdlib.h>
 #include <le/le.h>
 
 int
 main()
 {
-    LeTensor *sobel_filter = le_tensor_new(LE_TYPE_FLOAT32, 2, 3, 3,   
-        1.0, 0.0, -1.0,
-        2.0, 0.0, -2.0,
-        1.0, 0.0, -1.0
-    );
-
-    LeTensor *image = le_tensor_new(LE_TYPE_FLOAT32, 2, 6, 6,
+    LeTensor *input_image = le_tensor_new(LE_TYPE_FLOAT32, 2, 6, 6,
         0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
         0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
         0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
@@ -22,8 +18,23 @@ main()
         0.0, 0.0, 0.0, 1.0, 1.0, 1.0
     );
 
-    le_tensor_free(image);
+    LE_INFO("input_image = %s", le_tensor_to_cstr(input_image));
+
+    LeTensor *sobel_filter = le_tensor_new(LE_TYPE_FLOAT32, 2, 3, 3,   
+        1.0, 0.0, -1.0,
+        2.0, 0.0, -2.0,
+        1.0, 0.0, -1.0
+    );
+
+    LE_INFO("sobel_filter = %s", le_tensor_to_cstr(sobel_filter));
+
+    LeTensor *output_image = le_matrix_new_conv2d(input_image, sobel_filter);
+
+    LE_INFO("output_image = %s", le_tensor_to_cstr(output_image));
+
+    le_tensor_free(output_image);
     le_tensor_free(sobel_filter);
+    le_tensor_free(input_image);
 
     return EXIT_SUCCESS;
 }
