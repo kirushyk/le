18b993bb063774223f2f382359ccd4597f174f7c
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-02-19 16:00:43 +0200

    tensorlist: Implement version write

--- ext/tensorlist/letensorlist.c
+++ ext/tensorlist/letensorlist.c
@@ -1,9 +1,14 @@
 #include "letensorlist.h"
+#include <stdio.h>
+#include <stdint.h>
 
 void
 le_tensorlist_save(LeList *tensors, const char *filename)
 {
-
+    FILE *fout = fopen(filename, "wb");
+    uint8_t version = 0;
+    fwrite(&version, 1, 1, fout);
+    fclose(fout);
 }
 
 LeList *
