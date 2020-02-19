7f5ef594a084796396737461464f87eeed609a43
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-02-19 16:56:10 +0200

    tests: Add dummy tensorlist test

---
+++ tests/tensorlist.c
@@ -0,0 +1,12 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <le/le.h>
+
+int
+main()
+{
+    return EXIT_SUCCESS;
+}
