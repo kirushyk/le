437502b0b709360aa705941d9e424f7b93118670
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-02-19 16:13:31 +0200

    tensorlist: Add copyrights, tensor counter, version check and error logging

--- ext/tensorlist/letensorlist.c
+++ ext/tensorlist/letensorlist.c
@@ -1,6 +1,12 @@
+/* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
+   Released under the MIT license. See LICENSE file in the project root for full license information. */
+
+#define DEFAULT_LOG_CATEGORY "tensorlist"
+
 #include "letensorlist.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <le/letensor-imp.h>
 
 void
 le_tensorlist_save(LeList *tensors, const char *filename)
@@ -9,7 +15,16 @@ le_tensorlist_save(LeList *tensors, const char *filename)
     if (fout)
     {
         uint8_t version = 1;
-        fwrite(&version, 1, 1, fout);
+        fwrite(&version, sizeof(version), 1, fout);
+        uint16_t num_tensors = 0;
+        /// @todo: Make this be LeList function
+        for (LeList *current = tensors; current != NULL; current = current->next)
+            num_tensors++;
+        fwrite(&num_tensors, sizeof(num_tensors), 1, fout);
+        for (LeList *current = tensors; current != NULL; current = current->next)
+        {
+
+        }
         fclose(fout);
     }
 }
@@ -22,8 +37,25 @@ le_tensorlist_load(const char *filename)
     if (fin)
     {
         uint8_t version = 0;
-        fread(&version, 1, 1, fin);
+        fread(&version, sizeof(version), 1, fin);
+        if (version == 1)
+        {
+            uint16_t num_tensors = 0;
+            fread(&num_tensors, sizeof(num_tensors), 1, fin);
+            for (uint16_t i = 0; i < num_tensors; i++)
+            {
+
+            }
+        }
+        else
+        {
+            LE_WARNING("%s: Unknown version of .tensorlist file: %d", filename, (int)version);
+        }
         fclose(fin);
+    }
+    else
+    {
+        LE_WARNING("File not found: %s", filename);
     }
     return list;
 }
