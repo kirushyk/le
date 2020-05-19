424687a6084b1e5df611d3909d25f0b728e87863
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-19 07:54:50 +0300

    bindings: Remove vanilla Python bindings

--- python/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-lemodule = Extension('le', sources = ['lemodule.c'])
-
-setup (name = 'Le',
-       version = '0.2',
-       description = 'Machine Learning Framework',
-       ext_modules = [lemodule])
