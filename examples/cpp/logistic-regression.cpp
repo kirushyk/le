3848a91d06b56b6973dea2e1780540eb6b3e57b2
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-19 11:07:17 +0300

    examples/cpp/logistic-regression: Fix training data non-linearity

--- examples/cpp/logistic-regression.cpp
+++ examples/cpp/logistic-regression.cpp
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[])
 {
     le::Tensor x(le::Type::FLOAT32, 2, 2, 4,
-        1.0, 2.0, 1.0, 2.0,
-        2.0, 2.0, 1.0, 1.0
+        1.0, 2.0, 3.0, 4.0,
+        4.0, 3.0, 2.0, 1.0
     );
 
     le::Tensor y(le::Type::FLOAT32, 2, 1, 4,
-        0.0, 1.0, 1.0, 0.0
+        0.0, 0.0, 1.0, 1.0
     );
 
     std::cout << "Train set: " << std::endl;
