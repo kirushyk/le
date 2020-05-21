da0512a06ba24866fa487225c1d4ce4c7142fc8a
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-21 11:28:17 +0300

    bindings/python: Expose dummy Tensor class

--- bindings/python/lemodule.cpp
+++ bindings/python/lemodule.cpp
@@ -1,15 +1,18 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <le.hpp>
 
 namespace py = pybind11;
 
-int
-tensor()
+py::object tensor(py::array_t<double> elements)
 {
-    return 0;
+    le::Tensor t(le::Type::FLOAT32, 1, 1, 1.0);
+    return py::cast(t);
 }
 
 PYBIND11_MODULE(le, m)
 {
+    py::class_<le::Tensor>(m, "Tensor");
     m.doc() = "Le Python Binding";
     m.def("tensor", &tensor, "Create a Le Tensor");
 }
