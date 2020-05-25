40d08cad6ebede1c2dc3cc1446fd006cd1af4329
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-25 09:21:06 +0300

    tests: Add ReLU with signed integers case

--- tests/relu.c
+++ tests/relu.c
@@ -35,14 +35,35 @@ main()
 
     m = le_tensor_new(LE_TYPE_INT8, 2, 1, 1, 1);
     le_matrix_set(m, 0, 0, (int8_t)2);
+    assert(le_matrix_at_i8(m, 0, 0) == 2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i8(m, 0, 0) == 2);
+    le_matrix_set(m, 0, 0, (int8_t)-2);
+    assert(le_matrix_at_i8(m, 0, 0) == -2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i8(m, 0, 0) == 0);
     le_tensor_free(m);
 
     m = le_tensor_new(LE_TYPE_INT16, 2, 1, 1, 1);
     le_matrix_set(m, 0, 0, (int16_t)2);
+    assert(le_matrix_at_i16(m, 0, 0) == 2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i16(m, 0, 0) == 2);
+    le_matrix_set(m, 0, 0, (int16_t)-2);
+    assert(le_matrix_at_i16(m, 0, 0) == -2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i16(m, 0, 0) == 0);
     le_tensor_free(m);
 
     m = le_tensor_new(LE_TYPE_INT32, 2, 1, 1, 1);
     le_matrix_set(m, 0, 0, (int32_t)2);
+    assert(le_matrix_at_i32(m, 0, 0) == 2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i32(m, 0, 0) == 2);
+    le_matrix_set(m, 0, 0, (int32_t)-2);
+    assert(le_matrix_at_i32(m, 0, 0) == -2);
+    le_tensor_apply_relu(m);
+    assert(le_matrix_at_i32(m, 0, 0) == 0);
     le_tensor_free(m);
     
     return EXIT_SUCCESS;
