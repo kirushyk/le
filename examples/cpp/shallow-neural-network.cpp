ac36ced9aadd365a9b480873d6fb37a66a6b8c51
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-29 16:24:10 +0300

    examples/cpp: Invoke BGD algorithm

--- examples/cpp/shallow-neural-network.cpp
+++ examples/cpp/shallow-neural-network.cpp
@@ -16,12 +16,27 @@ int main(int argc, const char *argv[])
     std::cout << "x =" << std::endl << x << std::endl;
     std::cout << "y =" << std::endl << y << std::endl;
 
+    std::cout << "Creating Neural Network Structure" << std::endl;
     le::Sequential nn;
     nn.add(le::DenseLayer("D1", 2, 2));
     nn.add(le::ActivationLayer("A1", le::Activation::SIGMOID));
     nn.add(le::DenseLayer("D2", 2, 1));
     nn.add(le::ActivationLayer("A2", le::Activation::SIGMOID));
     nn.setLoss(le::Loss::LOGISTIC);
+
+    std::cout << "Training Neural Network" << std::endl;
+    le::BGD optimizer(nn, x, y, 3.f);
+    for (unsigned i = 0; i <= 1000; i++)
+    {
+        optimizer.step();
+
+        if ((i % 100) == 0)
+        {
+            std::cout << "Iteration " << i << std::endl;
+            le::Tensor h = nn.predict(x);
+            std::cout << "Training Error = " << le_logistic_loss(h.c_tensor(), y.c_tensor()) << std::endl;
+        }
+    }
 
     le::Tensor h = nn.predict(x);
     std::cout << "Predicted value =" << std::endl << h << std::endl;
