c739f4ce6b656e98b8e74f086a5c44e1915c9ac9
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-05-29 21:07:25 +0300

    bindings/cpp: Wrap BGD constructor

--- bindings/cpp/bgd.cpp
+++ bindings/cpp/bgd.cpp
@@ -4,12 +4,13 @@ using namespace le;
 
 BGD::BGD(Model model, Tensor input, Tensor output, float learning_rate)
 {
-
+    LeOptimizer *c_optimizer = LE_OPTIMIZER(le_bgd_new(model.c_model(), input.c_tensor(), output.c_tensor(), learning_rate));
+    setCOptimizer(c_optimizer);
 }
 
 BGD::~BGD()
 {
-    
+
 }
 
 void BGD::step()
