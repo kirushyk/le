ad0abaa3e7de06e37e4c336c4520a6dc6be6363a
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-10 17:27:23 +0300

    examples/python/shallow-neural-network: Format logistic loss output

--- examples/python/shallow-neural-network.py
+++ examples/python/shallow-neural-network.py
@@ -6,7 +6,7 @@ x = le.tensor([[1.0, 2.0, 1.0, 2.0],
 
 y = le.tensor([[0.0, 1.0, 1.0, 0.0]])
 
-print('Train set: ')
+print('Train set:')
 print('x =\n', x)
 print('y =\n', y)
 
@@ -16,5 +16,15 @@ nn.add(le.ActivationLayer('A1', le.Activation.SIGMOID))
 nn.add(le.DenseLayer('D2', 2, 1))
 nn.add(le.ActivationLayer('A2', le.Activation.SIGMOID))
 nn.setLoss(le.Loss.LOGISTIC)
+
+print('Training Neural Network')
+optimizer = le.BGD(nn, x, y, 3.0)
+for i in range(1000):
+    optimizer.step()
+    if i % 100 == 0:
+        print('Iteration', i)
+        h = nn.predict(x)
+        print('Training Error =', format(le.logistic_loss(h, y), '.3f'))
+
 h = nn.predict(x)
 print('Predicted value =\n', h)
