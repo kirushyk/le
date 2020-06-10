77ca0c9f0d21d368a4be75a1e1a1550a9ad0f5fb
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-10 17:13:19 +0300

    bindings/python: Expose Batched Gradient Descent

--- bindings/python/lemodule.cpp
+++ bindings/python/lemodule.cpp
@@ -90,6 +90,9 @@ PYBIND11_MODULE(le, m)
         .def("add", &le::Sequential::add)
         .def("setLoss", &le::Sequential::setLoss)
         .def("predict", &le::Sequential::predict);
+    py::class_<le::Optimizer>(m, "Optimizer");
+    py::class_<le::BGD, le::Optimizer>(m, "BGD")
+        .def(py::init<le::Model, le::Tensor, le::Tensor, float>(), py::arg("model"), py::arg("x"), py::arg("y"), py::arg("learning_rate") = 1.0f);
     m.doc() = "Le Python Binding";
     m.def("tensor", &tensor, "Create a Le Tensor");
 }
