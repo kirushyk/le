c82c069489b17cae5fcbbd8ef67079b9c017c738
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-17 03:33:14 +0300

    rand: Add distribution enum

--- le/math/lerand.c
+++ le/math/lerand.c
@@ -16,3 +16,17 @@ le_random_normal_f32(void)
     float u2 = le_random_uniform_f32();
     return cosf(2.0f * (float)M_PI * u2) * sqrtf(-2.0f * logf(u1));
 }
+
+float
+le_random_f32(LeDistribution distribution)
+{
+    switch (distribution)
+    {
+    case LE_DISTRIBUTION_NORMAL:
+        return le_random_normal_f32();
+    
+    case LE_DISTRIBUTION_UNIFORM:
+    default:
+        return le_random_uniform_f32();
+    }
+}
--- le/math/lerand.h
+++ le/math/lerand.h
@@ -9,6 +9,14 @@ float                   le_random_uniform_f32              (void);
 
 float                   le_random_normal_f32               (void);
 
+typedef enum LeDistribution
+{
+    LE_DISTRIBUTION_UNIFORM,
+    LE_DISTRIBUTION_NORMAL
+} LeDistribution;
+
+float                   le_random_f32                      (LeDistribution          distribution);
+
 LE_END_DECLS
 
 #endif
