cf062cf2b35395fa127994644ef07992089d6237
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-17 04:11:07 +0300

    tools/playground: Refrain from redefining M_PI

--- tools/playground/pg-generate-data.c
+++ tools/playground/pg-generate-data.c
@@ -5,10 +5,6 @@
 #include <glib.h>
 #include <math.h>
 
-#ifndef M_PI
-#   define M_PI (3.14159265358979323846)
-#endif
-
 LeDataSet *
 pg_generate_data(const char *pattern_name, unsigned examples_count)
 {
