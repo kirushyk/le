56e0cd0045fb40203f4b42a38181bef05a3c449b
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-18 13:54:52 +0300

    tests/gradcheck: Call actual gradient estimation

--- tests/gradcheck.c
+++ tests/gradcheck.c
@@ -19,10 +19,14 @@ main(int argc, char *argv[])
         0.0, 0.0, 1.0, 1.0
     );
     LeBGD *optimizer = le_bgd_new(LE_MODEL(nn), x, y, 1.0f);
-    for (unsigned i = 0; i <= 100; i++)
+    for (unsigned i = 0; i <= 10; i++)
     {
         le_optimizer_step(LE_OPTIMIZER(optimizer));
     }
+    LeList *gradients = le_model_get_gradients(LE_MODEL(nn), x, y);
+    LeList *gradients_estimations = le_sequential_estimate_gradients(nn, x, y);
+    le_list_foreach(gradients_estimations, (LeFunction)le_tensor_free);
+    le_list_foreach(gradients, (LeFunction)le_tensor_free);
     le_bgd_free(optimizer);
     le_tensor_free(y);
     le_tensor_free(x);
