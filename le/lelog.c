c25a965dd3cdfbee862a05148a45b3c550addd1d
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-18 17:15:15 +0300

    log: Add multiple debug categories support

--- le/lelog.c
+++ le/lelog.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <le/lelist.h>
 
 static const char *le_level_name[(size_t)LE_LOG_LEVEL_LAST] =
 {
@@ -13,13 +15,45 @@ static const char *le_level_name[(size_t)LE_LOG_LEVEL_LAST] =
     "INFO"
 };
 
+bool
+category_present(const char *category)
+{
+    char *le_debug = getenv("LE_DEBUG");
+    if (le_debug == NULL)
+        return true;
+
+    static bool categories_parsed = false;
+    static LeList *categories_requested = NULL;
+    if (!categories_parsed)
+    {
+        char *sep = ",";
+        char *word, *brkt;
+
+        for (word = strtok_r(le_debug, sep, &brkt);
+             word;
+             word = strtok_r(NULL, sep, &brkt))
+        {
+            categories_requested = le_list_prepend(categories_requested, word);
+        }
+
+        categories_parsed = true;
+    }
+    for (LeList *categories_iterator = categories_requested;
+         categories_iterator;
+         categories_iterator = categories_iterator->next)
+    {
+        if (strcmp((char *)categories_iterator->data, category) == 0)
+            return true;
+    }
+    return false;
+}
+
 void
 le_log(const char *category, LeLogLevel level, const char *message, ...)
 {
     if (level == LE_LOG_LEVEL_INFO)
     {
-        const char *le_debug = getenv("LE_DEBUG");
-        if ((le_debug == NULL) || strcmp(le_debug, category))
+        if (!category_present(category))
         {
             return;
         }
