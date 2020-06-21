646e817f7df9d78277a33c398003d64a2c0ab9e0
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-21 17:53:57 +0300

    platform: Add device type enum

---
+++ platform/ledevice.h
@@ -0,0 +1,16 @@
+#ifndef __LEDEVICE_H__
+#define __LEDEVICE_H__
+
+#include <le/lemacros.h>
+
+LE_BEGIN_DECLS
+
+typedef enum LeDeviceType
+{
+    LE_DEVICE_TYPE_CPU,
+    LE_DEVICE_TYPE_CUDA
+} LeDeviceType;
+
+LE_END_DECLS
+
+#endif
