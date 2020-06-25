34c0be23cd7c012ba4f6564026957414b98c7f53
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-25 11:50:13 +0300

    tensor: Store device type

--- le/tensors/letensor-imp.h
+++ le/tensors/letensor-imp.h
@@ -7,15 +7,17 @@
 #include <stdbool.h>
 #include "letype.h"
 #include "leshape.h"
+#include "../../platform/ledevice.h"
 
 struct LeTensor
 {
-    LeType    element_type;
-    LeShape  *shape;
-    bool      owns_data;
+    LeType        element_type;
+    LeShape      *shape;
+    bool          owns_data;
     /// @note: In dimension of lowest order
-    uint32_t  stride;
-    void     *data;
+    uint32_t      stride;
+    LeDeviceType  device_type;
+    void         *data;
 };
 
 #endif
