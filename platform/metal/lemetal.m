aca0a4914a202ae9cd247660d86a11875c9b3e8b
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-06-30 20:44:19 +0300

    metal: Fix matrix descriptors

--- platform/metal/lemetal.m
+++ platform/metal/lemetal.m
@@ -40,27 +40,24 @@ le_metal_matrix_new_product(const LeTensor *a, bool transpose_a, const LeTensor 
     size_t data_size = le_shape_get_elements_count(c->shape) * le_type_size(c->element_type);
     
     id <MTLBuffer> buff_a = (__bridge id<MTLBuffer>)a->data;
-    MPSMatrixDescriptor *desc_a = [[MPSMatrixDescriptor alloc] init];
-    [desc_a setRows: size_a];
-    [desc_a setColumns: c_width];
-    [desc_a setRowBytes: a->stride * sizeof(float)];
-    [desc_a setDataType: MPSDataTypeFloat32];
+    MPSMatrixDescriptor *desc_a = [MPSMatrixDescriptor matrixDescriptorWithRows:size_a
+        columns:c_width
+        rowBytes:a->stride * sizeof(float)
+        dataType:MPSDataTypeFloat32];
     MPSMatrix *mxa = [[MPSMatrix alloc] initWithBuffer:buff_a descriptor:desc_a];
     
     id <MTLBuffer> buff_b = (__bridge id<MTLBuffer>)b->data;
-    MPSMatrixDescriptor *desc_b = [[MPSMatrixDescriptor alloc] init];
-    [desc_b setRows: c_height];
-    [desc_b setColumns: size_b];
-    [desc_b setRowBytes: b->stride * sizeof(float)];
-    [desc_b setDataType: MPSDataTypeFloat32];
+    MPSMatrixDescriptor *desc_b = [MPSMatrixDescriptor matrixDescriptorWithRows:c_height
+        columns:size_b
+        rowBytes:b->stride * sizeof(float)
+        dataType:MPSDataTypeFloat32];
     MPSMatrix *mxb = [[MPSMatrix alloc] initWithBuffer:buff_b descriptor:desc_b];
     
     id <MTLBuffer> buff_c = [device newBufferWithLength:data_size options:0];
-    MPSMatrixDescriptor *desc_c = [[MPSMatrixDescriptor alloc] init];
-    [desc_c setRows: c_height];
-    [desc_c setColumns: c_width];
-    [desc_c setRowBytes: c->stride * sizeof(float)];
-    [desc_c setDataType: MPSDataTypeFloat32];
+    MPSMatrixDescriptor *desc_c = [MPSMatrixDescriptor matrixDescriptorWithRows:c_height
+        columns:c_height
+        rowBytes:c->stride * sizeof(float)
+        dataType:MPSDataTypeFloat32];
     MPSMatrix *mxc = [[MPSMatrix alloc] initWithBuffer:buff_c descriptor:desc_c];
     
     MPSMatrixMultiplication *kernel = [[MPSMatrixMultiplication alloc] initWithDevice: device
