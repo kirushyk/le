4dfa8ae00c8ed73de0c475fb7ebeaa7ad1064923
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-07-02 21:39:25 +0300

    metal: Small fixes

--- platform/metal/lemetal.m
+++ platform/metal/lemetal.m
@@ -40,23 +40,23 @@ le_metal_matrix_new_product(const LeTensor *a, bool transpose_a, const LeTensor 
     size_t data_size = le_shape_get_elements_count(c->shape) * le_type_size(c->element_type);
     
     id <MTLBuffer> buff_a = (__bridge id<MTLBuffer>)a->data;
-    MPSMatrixDescriptor *desc_a = [MPSMatrixDescriptor matrixDescriptorWithRows:size_a
-        columns:c_width
-        rowBytes:a->stride * sizeof(float)
+    MPSMatrixDescriptor *desc_a = [MPSMatrixDescriptor matrixDescriptorWithRows:c_height
+        columns:size_a
+        rowBytes:a->stride * le_type_size(c->element_type)
         dataType:MPSDataTypeFloat32];
     MPSMatrix *mxa = [[MPSMatrix alloc] initWithBuffer:buff_a descriptor:desc_a];
     
     id <MTLBuffer> buff_b = (__bridge id<MTLBuffer>)b->data;
-    MPSMatrixDescriptor *desc_b = [MPSMatrixDescriptor matrixDescriptorWithRows:c_height
-        columns:size_b
-        rowBytes:b->stride * sizeof(float)
+    MPSMatrixDescriptor *desc_b = [MPSMatrixDescriptor matrixDescriptorWithRows:size_b
+        columns:c_width
+        rowBytes:b->stride * le_type_size(c->element_type)
         dataType:MPSDataTypeFloat32];
     MPSMatrix *mxb = [[MPSMatrix alloc] initWithBuffer:buff_b descriptor:desc_b];
     
     id <MTLBuffer> buff_c = [device newBufferWithLength:data_size options:0];
     MPSMatrixDescriptor *desc_c = [MPSMatrixDescriptor matrixDescriptorWithRows:c_height
-        columns:c_height
-        rowBytes:c->stride * sizeof(float)
+        columns:c_width
+        rowBytes:c->stride * le_type_size(c->element_type)
         dataType:MPSDataTypeFloat32];
     MPSMatrix *mxc = [[MPSMatrix alloc] initWithBuffer:buff_c descriptor:desc_c];
     
@@ -78,16 +78,6 @@ le_metal_matrix_new_product(const LeTensor *a, bool transpose_a, const LeTensor 
     
     c->data = (void *)CFBridgingRetain(buff_c);
     
-//    cblas_sgemm(CblasRowMajor,
-//                transpose_a ? CblasTrans : CblasNoTrans,
-//                transpose_b ? CblasTrans : CblasNoTrans,
-//                c_height, c_width, size_a,
-//                1.0f,
-//                a->data, a->shape->sizes[1],
-//                b->data, b->shape->sizes[1],
-//                0.0f,
-//                c->data, c->shape->sizes[1]);
-//
     return c;
 }
 
