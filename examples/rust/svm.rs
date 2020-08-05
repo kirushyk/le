e28eafba4d71b8368c6190d5871aa0098c4bd1bb
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-08-05 13:35:01 +0300

    examples/rust: Update SVM demo

--- examples/rust/svm.rs
+++ examples/rust/svm.rs
@@ -4,6 +4,24 @@
 use le::Tensor;
 
 fn main() {
-    let x = Tensor::new();
+    let x = Tensor::new([[1.0, 2.0, 3.0, 4.0],
+                         [4.0, 3.0, 2.0, 1.0]]);
+
+    let y = Tensor::new([[-1.0, -1.0, 1.0, 1.0]]);
+    
+    print!("Train set:");
     print!("x = {}", x);
+    print!("x = {}", y);
+
+    let svm = SVM::new();
+    let mut options = SVM::TrainingOptions {
+        kernel: 0.9,
+        c: 1.0
+    }
+    svm.train(x, y, options);
+    
+    let h = svm.predict(x);
+    print!("Predicted value = {}", h);
+    
+    return 0;
 }
