e4cb677bcc78295a0ea6095c2ed684edf9dba37d
Kyrylo Polezhaiev <kirushyk@gmail.com>
2020-12-09 21:31:21 +0200

    tensor: Rename include guard

--- le/tensors/letensor-imp.h
+++ le/tensors/letensor-imp.h
@@ -1,8 +1,8 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
-#ifndef __LEMATRIX_IMP_H__
-#define __LEMATRIX_IMP_H__
+#ifndef __LETENSOR_IMP_H__
+#define __LETENSOR_IMP_H__
 
 #include <stdbool.h>
 #include "letype.h"
