5b31989d533639edae91a2794e17f3c03e6b3780
Kyrylo Polezhaiev <kirushyk@gmail.com>
2021-10-21 19:22:29 +0300

    platform/cuda: Fix #include paths

--- platform/cuda/lecuda.c
+++ platform/cuda/lecuda.c
@@ -1,10 +1,10 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
-#include "leaccelerate.h"
+#include "lecuda.h"
 #include <assert.h>
 #include <le/le.h>
-#include <le/letensor-imp.h>
+#include <le/tensors/letensor-imp.h>
 #include <cuda_runtime.h>
 
 LeTensor *
