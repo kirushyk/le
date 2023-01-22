0958f1b97534a7ca101c552b181c648c2f0bd5d4
Kyrylo Polezhaiev <kirushyk@gmail.com>
2023-01-22 02:35:36 +0000

    tests: Cover sigmoid on CUDA

---
+++ tests/cuda/sigmoid.c
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <le/le.h>
+#include <platform/cuda/lecuda.h>
+
+int main(int argc, char *argv[])
+{   
+    LeTensor *a = le_tensor_new(LE_TYPE_FLOAT32, 2, 4, 4,
+        0.0, 0.1, 0.2, 0.3,
+        0.4, 0.5, 0.6, 0.7,
+        0.8, 0.9, 1.0, 1.1,
+        1.2, 1.3, 1.4, 1.5
+    );
+    LeTensor *ca = le_tensor_to_cuda(a);
+    le_tensor_apply_sigmoid(a);
+    le_tensor_apply_sigmoid(ca);
+    LeTensor *result = le_cuda_tensor_to_cpu(ca);
+    // le_tensor_print(result, stdout);
+    // le_tensor_print(a, stdout);
+
+    le_tensor_sub(result, a);
+    float l2 = le_tensor_l2_f32(result);
+    assert(l2 < 1e-4f);
+
+    le_tensor_free(result);
+    le_tensor_free(ca);
+    le_tensor_free(a);
+
+    return EXIT_SUCCESS;
+}
