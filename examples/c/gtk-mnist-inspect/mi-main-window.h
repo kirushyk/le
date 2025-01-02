301f9ab934cd90ab5511a538e1afaa49c976a876
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-03 00:31:40 +0100

    examples/c: Format code better

--- examples/c/gtk-mnist-inspect/mi-main-window.h
+++ examples/c/gtk-mnist-inspect/mi-main-window.h
@@ -2,9 +2,9 @@
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
 #ifndef _LE_MAIN_WINDOW_
-#   define _LE_MAIN_WINDOW_
-#   include <gtk/gtk.h>
+#define _LE_MAIN_WINDOW_
+#include <gtk/gtk.h>
 
-GtkWidget * le_main_window_new (GtkApplication *app);
+GtkWidget *le_main_window_new (GtkApplication *app);
 
 #endif
--- examples/c/matrix-product.c
+++ examples/c/matrix-product.c
@@ -1,32 +1,32 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
-#include <stdio.h>
 #include <le/le.h>
+#include <stdio.h>
 
 int
-main(int argc, const char *argv[])
+main (int argc, const char *argv[])
 {
-    LeTensor *a = le_matrix_new_rand_f32(LE_DISTRIBUTION_UNIFORM, 4, 2);
-    printf("a =\n");
-    le_tensor_print(a, stdout);
-    
-    LeTensor *b = le_matrix_new_rand_f32(LE_DISTRIBUTION_UNIFORM, 2, 4);
-    printf("b =\n");
-    le_tensor_print(b, stdout);
-    
-    LeTensor *c = le_matrix_new_product(a, b);
-    printf("c = a * b =\n");
-    le_tensor_print(c, stdout);
-    
-    LeTensor *d = le_matrix_new_transpose(c);
-    printf("d = c' =\n");
-    le_tensor_print(d, stdout);
-    
-    le_tensor_free(d);
-    le_tensor_free(c);
-    le_tensor_free(b);
-    le_tensor_free(a);
-    
-    return 0;
+  LeTensor *a = le_matrix_new_rand_f32 (LE_DISTRIBUTION_UNIFORM, 4, 2);
+  printf ("a =\n");
+  le_tensor_print (a, stdout);
+
+  LeTensor *b = le_matrix_new_rand_f32 (LE_DISTRIBUTION_UNIFORM, 2, 4);
+  printf ("b =\n");
+  le_tensor_print (b, stdout);
+
+  LeTensor *c = le_matrix_new_product (a, b);
+  printf ("c = a * b =\n");
+  le_tensor_print (c, stdout);
+
+  LeTensor *d = le_matrix_new_transpose (c);
+  printf ("d = c' =\n");
+  le_tensor_print (d, stdout);
+
+  le_tensor_free (d);
+  le_tensor_free (c);
+  le_tensor_free (b);
+  le_tensor_free (a);
+
+  return 0;
 }
