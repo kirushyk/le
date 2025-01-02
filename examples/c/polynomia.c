7ef3548da2b18bb94150fab5fd110389603e2b61
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-03 00:36:09 +0100

    examples/c/polynomia: Format code better

--- examples/c/polynomia.c
+++ examples/c/polynomia.c
@@ -1,27 +1,27 @@
 /* Copyright (c) Kyrylo Polezhaiev and contributors. All rights reserved.
    Released under the MIT license. See LICENSE file in the project root for full license information. */
 
-#include <stdio.h>
 #include <le/le.h>
+#include <stdio.h>
 
 int
-main(int argc, const char *argv[])
+main (int argc, const char *argv[])
 {
-    LeTensor *a = le_matrix_new_rand_f32(LE_DISTRIBUTION_UNIFORM, 2, 4);
-    printf("a =\n");
-    le_tensor_print(a, stdout);
-    
-    LeTensor *b = le_matrix_new_polynomia(a);
-    printf("poly(a) =\n");
-    le_tensor_print(b, stdout);
-    
-    LeTensor *c = le_matrix_new_polynomia(b);
-    printf("poly(poly(a)) =\n");
-    le_tensor_print(c, stdout);
-    
-    le_tensor_free(c);
-    le_tensor_free(b);
-    le_tensor_free(a);
-    
-    return 0;
+  LeTensor *a = le_matrix_new_rand_f32 (LE_DISTRIBUTION_UNIFORM, 2, 4);
+  printf ("a =\n");
+  le_tensor_print (a, stdout);
+
+  LeTensor *b = le_matrix_new_polynomia (a);
+  printf ("poly(a) =\n");
+  le_tensor_print (b, stdout);
+
+  LeTensor *c = le_matrix_new_polynomia (b);
+  printf ("poly(poly(a)) =\n");
+  le_tensor_print (c, stdout);
+
+  le_tensor_free (c);
+  le_tensor_free (b);
+  le_tensor_free (a);
+
+  return 0;
 }
