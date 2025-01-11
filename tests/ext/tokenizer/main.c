87585e7c37fc733e7980bcda84ddeb5a6065aabe
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-11 19:51:28 +0100

    tests/ext: Run in source workdir

--- tests/ext/tokenizer/main.c
+++ tests/ext/tokenizer/main.c
@@ -4,7 +4,8 @@
 int
 main ()
 {
-  LeTokenizer *tokenizer = le_tokenizer_new ("test.txt");
+  LeTokenizer *tokenizer = le_tokenizer_new ("tokenizer.json");
+  g_assert_nonnull (tokenizer);
   g_object_unref (tokenizer);
   return EXIT_SUCCESS;
 }
