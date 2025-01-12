47a51ca2a755d5fa6b9b22d34d951d246768ba13
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-12 15:03:48 +0100

    tests/ext/tokenizer: Cover decoding

--- tests/ext/tokenizer/main.c
+++ tests/ext/tokenizer/main.c
@@ -1,11 +1,20 @@
 #include <stdlib.h>
+#include <glib.h>
 #include <ext/tokenizer/letokenizer.h>
 
 int
 main ()
 {
+  GList *tokens = NULL;
+  tokens = g_list_prepend (tokens, GINT_TO_POINTER (10));
+  tokens = g_list_prepend (tokens, GINT_TO_POINTER (20));
+  tokens = g_list_prepend (tokens, GINT_TO_POINTER (30));
   LeTokenizer *tokenizer = le_tokenizer_new ("tokenizer.json");
   g_assert_nonnull (tokenizer);
+  gchar *string = le_tokenizer_decode (tokenizer, tokens);
+  g_print ("%s\n", string);
+  g_free (string);
+  g_list_free (tokens);
   g_object_unref (tokenizer);
   return EXIT_SUCCESS;
 }
