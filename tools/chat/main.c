07fce6f7a4db3fb32aad6c3e922f0daf10b1b7cc
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-18 23:27:45 +0100

    tools/chat: Link against tokenizer lib

--- tools/chat/main.c
+++ tools/chat/main.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <glib.h>
+#include <ext/tokenizer/letokenizer.h>
 #include <readline/history.h>
 #include <readline/readline.h>
 
 int
 main (int argc, char *argv[])
 {
+  static gchar *tokenizer_config_filename = NULL;
   static GOptionEntry option_entries[] = { // clang-format off
-    { "model", 'm', 0, G_OPTION_ARG_STRING, NULL, "Model filename", "FILENAME" },
+    { "tokenizer-config", 't', 0, G_OPTION_ARG_STRING, &tokenizer_config_filename, "Tokenizer config", "TOKENIZER.JSON" },
     { NULL }
   }; // clang-format on
   g_set_prgname ("le-chat");
@@ -18,10 +20,17 @@ main (int argc, char *argv[])
   GError *error = NULL;
   if (!g_option_context_parse (option_context, &argc, &argv, &error)) {
     g_assert_nonnull (error);
-    g_print ("%s\n", error->message);
+    g_printerr ("%s\n", error->message);
     g_error_free (error);
     return EXIT_FAILURE;
   }
+
+  LeTokenizer *tokenizer = le_tokenizer_new (tokenizer_config_filename);
+  if (tokenizer == NULL) {
+    g_printerr ("Can not initialize tokenizer.\n");
+    return EXIT_FAILURE;
+  }
+
   while (TRUE) {
     gchar *prompt = readline ("> ");
     if (prompt == NULL) {
@@ -29,6 +38,9 @@ main (int argc, char *argv[])
     }
     add_history (prompt);
   }
+
+  g_object_unref (tokenizer);
+
   g_option_context_free (option_context);
   return EXIT_SUCCESS;
 }
