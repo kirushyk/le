e18abd688cb2ad00b4afc953f9a113d2a29bdfc4
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-18 17:44:46 +0100

    ext/tokenizer: Implement BPE merging and encoding

--- ext/tokenizer/letokenizer.c
+++ ext/tokenizer/letokenizer.c
@@ -1,29 +1,21 @@
 #include "letokenizer.h"
 #include <json-glib/json-glib.h>
 
+typedef struct MergeIndices
+{
+  guint32 first;
+  guint32 second;
+  guint32 merged;
+} MergeIndices;
+
 struct _LeTokenizer
 {
   GObject parent;
   GHashTable *text_to_id;
   GHashTable *id_to_text;
-  GList *merges;
+  gsize num_merge_pairs;
+  MergeIndices *merge_indices;
 };
-
-typedef struct MergePair
-{
-  gchar *first;
-  gchar *second;
-} MergePair;
-
-void
-merge_pair_free (MergePair *pair)
-{
-  if (pair) {
-    g_free (pair->first);
-    g_free (pair->second);
-  }
-  g_free (pair);
-}
 
 static void le_tokenizer_class_init (LeTokenizerClass *klass);
 static void le_tokenizer_init (LeTokenizer *self);
@@ -38,7 +30,8 @@ le_tokenizer_dispose (GObject *object)
   g_hash_table_unref (self->text_to_id);
   g_assert_nonnull (self->id_to_text);
   g_hash_table_unref (self->id_to_text);
-  g_list_free_full (self->merges, (GDestroyNotify)merge_pair_free);
+  if (self->merge_indices)
+    g_free (self->merge_indices);
   G_OBJECT_CLASS (le_tokenizer_parent_class)->dispose (object);
 }
 
@@ -85,27 +78,32 @@ le_tokenizer_new (const gchar *filename)
   self->text_to_id = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, NULL);
   self->id_to_text = g_hash_table_new_full (g_direct_hash, g_direct_equal, NULL, g_free);
 
-  GList *vocam_members = json_object_get_members (vocab_object);
-  for (GList *iter = vocam_members; iter != NULL; iter = iter->next) {
+  GList *vocab_members = json_object_get_members (vocab_object);
+  for (GList *iter = vocab_members; iter != NULL; iter = iter->next) {
     const gchar *key = (const gchar *)iter->data;
     gint64 id = json_object_get_int_member (vocab_object, key);
     g_hash_table_insert (self->text_to_id, g_strdup (key), GINT_TO_POINTER (id));
     g_hash_table_insert (self->id_to_text, GINT_TO_POINTER (id), g_strdup (key));
   }
-  g_list_free (vocam_members);
+  g_list_free (vocab_members);
 
-  self->merges = NULL;
+  self->num_merge_pairs = 0;
   JsonArray *merges_array = json_object_get_array_member (model_object, "merges");
-  if (merges_array != NULL) {
-    for (gsize i = 0; i < json_array_get_length (merges_array); i++) {
-      const gchar *merge_string = json_array_get_string_element (merges_array, i);
-      if (merge_string != NULL) {
-        gchar **merge_item = g_strsplit (merge_string, " ", 2);
-        MergePair *pair = g_new (MergePair, 1);
-        pair->first = g_strdup (merge_item[0]);
-        pair->second = g_strdup (merge_item[1]);
-        g_strfreev (merge_item);
-        self->merges = g_list_prepend (self->merges, pair);
+  if (merges_array) {
+    self->num_merge_pairs = json_array_get_length (merges_array);
+    self->merge_indices = g_new (MergeIndices, self->num_merge_pairs);
+    if (merges_array != NULL) {
+      for (gsize i = 0; i < self->num_merge_pairs; i++) {
+        const gchar *merge_string = json_array_get_string_element (merges_array, i);
+        if (merge_string != NULL) {
+          gchar **merge_item = g_strsplit (merge_string, " ", 2);
+          self->merge_indices[i].first = GPOINTER_TO_INT (g_hash_table_lookup (self->text_to_id, merge_item[0]));
+          self->merge_indices[i].second = GPOINTER_TO_INT (g_hash_table_lookup (self->text_to_id, merge_item[1]));
+          gchar *merged_token = g_strjoinv ("", merge_item);
+          self->merge_indices[i].merged = GPOINTER_TO_INT (g_hash_table_lookup (self->text_to_id, merged_token));
+          g_free (merged_token);
+          g_strfreev (merge_item);
+        }
       }
     }
   }
@@ -127,7 +125,22 @@ le_tokenizer_encode (LeTokenizer *self, const gchar *text)
     }
     g_free (token);
   }
-  return g_list_reverse (tokens);
+  tokens = g_list_reverse (tokens);
+  for (GList *iter = tokens; iter != NULL; iter = iter->next) {
+    GList *next_iter = iter->next;
+    if (next_iter != NULL) {
+      gint64 id = GPOINTER_TO_INT (iter->data);
+      gint64 next_id = GPOINTER_TO_INT (next_iter->data);
+      for (gsize i = 0; i < self->num_merge_pairs; i++) {
+        if (self->merge_indices[i].first == id && self->merge_indices[i].second == next_id) {
+          iter->data = GINT_TO_POINTER (self->merge_indices[i].merged);
+          tokens = g_list_delete_link (tokens, next_iter);
+          break;
+        }
+      }
+    }
+  }
+  return tokens;
 }
 
 gchar *
