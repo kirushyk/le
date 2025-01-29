97852c45f905cea54f9dc31699a2867191b6b3be
Kyrylo V. Polezhaiev <kirushyk@gmail.com>
2025-01-29 04:54:58 +0100

    backends/cpu: Link against Accelerate.framework and OpenBLAS, add dot product

--- backends/cpu/lecpu.c
+++ backends/cpu/lecpu.c
@@ -1,5 +1,14 @@
 #include "lecpu.h"
+#include "cpu-backend-config.h"
 #include "../../le/lebackend.h"
+#include "../../le/tensors/letensor-imp.h"
+
+#ifdef HAVE_ACCELERATE_FRAMEWORK
+#  include <Accelerate/Accelerate.h>
+#endif
+#ifdef HAVE_OPENBLAS
+#  include <cblas.h>
+#endif
 
 struct _LeCpuBackend
 {
@@ -14,6 +23,14 @@ le_cpu_backend_class_init (LeCpuBackendClass *klass)
 static void
 le_cpu_backend_init (LeCpuBackend *self)
 {
+}
+
+gfloat
+le_cblas_dot_product (const LeTensor *a, const LeTensor *b)
+{
+#if defined(HAVE_ACCELERATE_FRAMEWORK) || defined(HAVE_OPENBLAS)
+  return cblas_sdot (a->shape->sizes[0], a->data, a->stride, b->data, b->stride);
+#endif
 }
 
 static void
@@ -33,9 +50,9 @@ le_cpu_backend_get_instance (void)
 
   if (g_once_init_enter (&init_guard)) {
     self = g_object_new (le_cpu_backend_get_type (), NULL);
-    g_object_add_weak_pointer (G_OBJECT (self), &self);
+    g_object_add_weak_pointer (G_OBJECT (self), (gpointer *)&self);
     g_once_init_leave (&init_guard, (gsize)self);
   }
-  
+
   return g_object_ref_sink (self);
 }
